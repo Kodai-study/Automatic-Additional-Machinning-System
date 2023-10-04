c8c95b7441c1c8f6296686e15ed0978b43218723
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-04 19:32:02 +0900

    通信クラスだけ別ファイルに移動

---
+++ RobotCommunication.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from enum import Enum
+from queue import Queue
+
+
+class RobotCommunicationHandler:
+    """
+    ロボットとの通信を管理するクラス
+    常にロボットとのソケット接続を確立しておき、
+    送信要求や受信に関する処理を行う
+    """
+
+    def __init__(self):
+        self.send_queue = None
+        self.receive_queue = None
+
+    def communication_loop(self, send_queue: Queue, receive_queue: Queue):
+        """
+        受信する、統合スレッドからの送信要求の待ち受けのループを開始する
+
+        Args:
+            send_queue (Queue): 統合スレッドに送るデータを入れるキュー\n
+            receive_queue (Queue): 統合スレッドから受け取ったデータを入れるキュー
+        """
+        self.send_queue = send_queue
+        self.receive_queue = receive_queue
