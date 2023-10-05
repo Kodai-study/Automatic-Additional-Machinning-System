284c1a9c50fa32cdb949636e6daa1b64332547c9
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-05 17:31:48 +0900

    複数ソケットに対応
    通信先が2つあるので、2つの通信先に送信ができるように変更

--- RobotCommunication.py
+++ RobotCommunication.py
@@ -7,7 +7,12 @@ from test_receiv import receive
 import socket
 import time
 
-PORT = 5000
+TEST_PORT = 5000
+TEST_HOST = 'localhost'
+TEST_PORT1 = 5000
+TEST_PORT2 = 5001
+
+TEST_stop_flag = False
 
 
 class RobotCommunicationHandler:
@@ -20,6 +25,19 @@ class RobotCommunicationHandler:
     def __init__(self):
         self.send_queue = None
         self.receive_queue = None
+        self.samp_stop_flag = False
+
+    def receive_string(self, sock: socket.socket) -> str:
+        while not TEST_stop_flag:
+            try:
+                data = sock.recv(1024)
+                if not data:
+                    print("Connection closed by the server")
+                    break
+                print(f"Received: {data.decode('utf-8')}")
+            except Exception as e:
+                print(f"Error: {e}")
+                break
 
     def communication_loop(self, send_queue: Queue, receive_queue: Queue):
         """
@@ -32,28 +50,35 @@ class RobotCommunicationHandler:
         self.send_queue = send_queue
         self.receive_queue = receive_queue
 
-        HOST = 'localhost'
+        # UR、CFD用の2つのソケットの作成(現在はサンプル)
+        try:
+            self.samp_socket1 = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+            self.samp_socket2 = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-            print('Connected to', HOST, 'on port', PORT)
-            while True:
-                # send_queueに値が入っているか監視
-                if not self.send_queue.empty():
-                    # send_queueから値を取り出す
-                    send_data = self.send_queue.get()
-                    # 取り出した値を接続先に送信
-                    s.sendall(send_data.encode('utf-8'))
-                    print('Sent:', send_data)
+            self.samp_socket1.connect((TEST_HOST, TEST_PORT1))
+            self.samp_socket2.connect((TEST_HOST, TEST_PORT2))
+        except Exception as e:
+            print('Socket Error: ', e)
 
+        # 2つのソケットと同時に通信するためのスレッドを2つ作成
+        receive_thread1 = Thread(
+            target=self.receive_string, args=(self.samp_socket1,))
+        receive_thread1.start()
+        receive_thread2 = Thread(
+            target=self.receive_string, args=(self.samp_socket2,))
+        receive_thread2.start()
 
-                if not self.receive_queue.empty():
-                    # receive_queueから値を取り出す
-                    receive_data = self.receive_queue.get()
-                    print('receiv :', receive_data)
-                    if receive_data == "exit":
-                        break
-                time.sleep(0.1)
+        while not TEST_stop_flag:
+            # send_queueに値が入っているか監視
+            if not self.send_queue.empty():
+                # send_queueから値を取り出す
+                send_data = self.send_queue.get()
+                # 取り出した値を接続先に送信
+                self.samp_socket1.sendall(send_data.encode('utf-8'))
+                self.samp_socket2.sendall(send_data.encode('utf-8'))
+                print('Sent:', send_data)
 
 
 if __name__ == '__main__':
@@ -61,8 +86,11 @@ if __name__ == '__main__':
     communication_handler = RobotCommunicationHandler()
 
     # 別スレッドで、自身からの通信を待ち受けるサーバプログラムを起動する
-    receive_thread = Thread(target=receive, args=(PORT,))
-    receive_thread.start()
+    receive_thread1 = Thread(target=receive, args=(TEST_PORT1,))
+    receive_thread2 = Thread(target=receive, args=(TEST_PORT2,))
+    receive_thread1.start()
+    receive_thread2.start()
+
     send_queue = Queue()
     receive_queue = Queue()
     communication_thread = Thread(
@@ -73,6 +101,8 @@ if __name__ == '__main__':
         send_data = f"hello_{i}"
         send_queue.put(send_data)
         time.sleep(0.1)
-    receive_thread.join()
+    receive_thread1.join()
+    receive_thread2.join()
     receive_queue.put("exit")
+    TEST_stop_flag = True
     print("receive_thread joined")
