a53bd4455e90c56c2b5e3076a1afcf44eef28609
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-05 13:28:48 +0900

    送受信のテスト作成
    RobotCommunication.py のメイン処理で、通信クラスの送信処理のテストを書いた

--- RobotCommunication.py
+++ RobotCommunication.py
@@ -1,6 +1,13 @@
 # coding: utf-8
 from enum import Enum
 from queue import Queue
+import socket
+from threading import Thread
+from test_receiv import receive
+import socket
+import time
+
+port = 5000
 
 
 class RobotCommunicationHandler:
@@ -24,3 +31,40 @@ class RobotCommunicationHandler:
         """
         self.send_queue = send_queue
         self.receive_queue = receive_queue
+
+        HOST = 'localhost'
+        PORT = 5000
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            print('Connected to', HOST, 'on port', PORT)
+            while True:
+                # send_queueに値が入っているか監視
+                if not self.send_queue.empty():
+                    # send_queueから値を取り出す
+                    send_data = self.send_queue.get()
+                    # 取り出した値を接続先に送信
+                    s.sendall(send_data.encode('utf-8'))
+                    print('Sent:', send_data)
+                time.sleep(0.1)
+
+
+if __name__ == '__main__':
+    # ポート番号をコマンドライン引数から取得する
+    communication_handler = RobotCommunicationHandler()
+
+    # 別スレッドで、自身からの通信を待ち受けるサーバプログラムを起動する
+    receive_thread = Thread(target=receive, args=(port,))
+    receive_thread.start()
+    send_queue = Queue()
+    receive_queue = Queue()
+    communication_thread = Thread(
+        target=communication_handler.communication_loop, args=(send_queue, receive_queue))
+
+    communication_thread.start()
+    for i in range(10):
+        send_data = f"hello_{i}"
+        send_queue.put(send_data)
+        time.sleep(0.1)
+    receive_thread.join()
+    communication_thread.join()
