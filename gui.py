9f94f18d30e6825647b8c746f9a8e172f17ef38c
Shiraishi Akira <a22ei11@hokuriku-pc.ac.jp>
2023-10-16 11:46:27 +0900

    確認画面まで

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import simpledialog
+from itertools import cycle
 import tkinter.font as f
 
 
@@ -56,7 +57,7 @@ class NumberPad(tk.Toplevel):
 class Application:
     def __init__(self, root):
         self.root = root
-        self.root.title("ファイル選択とデータ入力")
+        self.root.title("T5GUI")
         self.root.geometry("1920x1080+0+0")  # ウィンドウサイズと位置を設定
 
         self.selection_frame = None  # フレームを初期化
@@ -70,12 +71,16 @@ class Application:
         style.configure("Treeview.Heading", font=("AR丸ゴシック体M", 24))
         style.configure("Treeview", font=("AR丸ゴシック体M", 18), rowheight=40)
 
+        self.red_lamp_img = tk.PhotoImage(file="./resource/images/red_lamp.png")
+        self.green_lamp_img = tk.PhotoImage(file="./resource/images/green_lamp.png")
+        self.current_img = self.red_lamp_img  # 最初はred_lamp.pngを表
+
+        # 初期画像を設定
+        # initial_image = next(image_cycle)
+        # self.img = tk.PhotoImage(file=initial_image)
+
         # ログイン画面を表示lf_login_frame
         self.create_login_frame()
-
-    def clear_frame(self):
-        if self.selection_frame:
-            self.selection_frame.destroy()
 
     def create_login_frame(self):
         # ログイン画面を作成
@@ -118,9 +123,13 @@ class Application:
         login_button.pack()
         login_button.place(x=1270, y=550)
 
-    def create_selection_frame(self):
+    def create_selection_frame(self, selected_items=None):
+
+        if hasattr(self, 'check_frame') and self.check_frame:
+            self.check_frame.destroy()
 
         self.selection_frame = tk.Frame(self.root)
+
         # データを表示する表（Treeview）
         self.table = ttk.Treeview(self.selection_frame, columns=(
             "Data", "Quantity"), show="headings")
@@ -142,24 +151,27 @@ class Application:
 
         # ボタン
         go_monitor_button = tk.Button(self.selection_frame, text="モニタ画面",
-                                    command=self.create_monitor_frame, font=("AR丸ゴシック体M", 18), width=22)
+                                      command=self.create_monitor_frame, font=("AR丸ゴシック体M", 18), width=22)
         go_check_button = tk.Button(self.selection_frame, text="確認画面",
                                     command=lambda: self.create_check_frame(self.data_list), font=("AR丸ゴシック体M", 18), width=22)
         add_data_button = tk.Button(self.selection_frame, text="ファイル参照",
                                     command=self.add_data_from_file, font=("AR丸ゴシック体M", 18), width=22)
 
+        # データを再描画
+        if selected_items:
+            for data, quantity in selected_items:
+                self.table.insert("", "end", values=(data, quantity))
+
         # ウィジェットの配置
         self.selection_frame.pack(fill="both", expand=True)
-        self.table.place(relheigh=0.6, relwidth=0.7, x=130,y=70)
-        scrollbar.place(relheigh=0.6, x=1464,y=70)
-        add_data_button.place(rely=0.2, x=1530,y=200)
+        self.table.place(relheigh=0.6, relwidth=0.7, x=130, y=70)
+        scrollbar.place(relheigh=0.6, x=1464, y=70)
+        add_data_button.place(rely=0.2, x=1530, y=200)
         go_monitor_button.place(rely=0.85, relx=0.1)
         go_check_button.place(rely=0.85, relx=0.75)
 
-        self.selection_frame.pack()  # フレームを表示
-
+    # ファイルを参照してデータを追加
     def add_data_from_file(self):
-        # ファイルを参照してデータを追加
         file_path = filedialog.askopenfilename(
             title="ファイルを選択してください", filetypes=(("テキストファイル", "*.txt"), ("すべてのファイル", "*.*")))
         if file_path:
@@ -181,46 +193,71 @@ class Application:
                 table.insert("", "end", values=(data, quantity))
 
     def create_check_frame(self, selected_items):
+
         if self.selection_frame:
             self.selection_frame.destroy()
 
         self.check_frame = tk.Frame(self.root)
 
-        label = tk.Label(self.check_frame, text="選択した加工データ:", font=("AR丸ゴシック体M", 24))
-        decoy_label = tk.Label(self.check_frame, text="                                                 ", 
-                                    font=("AR丸ゴシック体M", 24))
+        label = tk.Label(self.check_frame, text="選択した加工データ:",
+                         font=("AR丸ゴシック体M", 24))
+        decoy_label = tk.Label(self.check_frame, text="                                                 ",
+                               font=("AR丸ゴシック体M", 24))
+        label_lamp = tk.Label(self.check_frame, image=self.current_img)
 
         # リストボックスを作成
-        listbox = tk.Listbox(self.check_frame, font=("AR丸ゴシック体M", 18), selectmode=tk.MULTIPLE, width=80,height=30, justify="center")
-        
+        listbox = tk.Listbox(self.check_frame, font=(
+            "AR丸ゴシック体M", 18), selectmode=tk.MULTIPLE, width=80, height=25, justify="center")
+
         for item in selected_items:
             listbox.insert(tk.END, f"{item[0]} - 個数: {item[1]}")
 
         # スクロールバーを作成し、リストボックスに配置
-        scrollbar = tk.Scrollbar(self.check_frame, orient=tk.VERTICAL, command=listbox.yview)
+        scrollbar = tk.Scrollbar(
+            self.check_frame, orient=tk.VERTICAL, command=listbox.yview)
         listbox.config(yscrollcommand=scrollbar.set)
-        
-        def confirm_selected_data():
-            selected_indices = listbox.curselection()
-            for index in selected_indices:
-                item = selected_items[int(index)]
-                print(f"選択したデータ: {item[0]}, 個数: {item[1]}")
 
-        confirm_button = tk.Button(self.check_frame, text="確認", command=confirm_selected_data, font=("AR丸ゴシック体M", 24))
-        
+        # 　準備完了ボタンとランプ変更
+        def toggle_ready_state():
+            if ready_button["text"] == "準備完了":
+                ready_button["text"] = "準備取り消し"
+                self.current_img = self.green_lamp_img
+            else:
+                ready_button["text"] = "準備完了"
+                self.current_img = self.red_lamp_img
+            label_lamp.config(image=self.current_img)
+            label_lamp.image = self.current_img
+
+        # ボタンを作成
+        back_button = tk.Button(
+            self.check_frame, text="戻る", command=self.back_to_selection_frame, font=("AR丸ゴシック体M", 18), width=22)
+        go_monitor_button = tk.Button(
+            self.check_frame, text="モニタ画面", command=self.create_monitor_frame, font=("AR丸ゴシック体M", 18), width=22)
+        ready_button = tk.Button(
+            self.check_frame, text="準備完了", command=toggle_ready_state, font=("AR丸ゴシック体M", 22), width=24)
+
         # ウィジェットの配置
         self.check_frame.pack(fill="both", expand=True)
         decoy_label.grid(row=0, column=0)
-        label.grid(row=0, column=1,pady=40)
+        label.grid(row=0, column=1, pady=40)
         listbox.grid(row=1, column=1)
         scrollbar.grid(row=1, column=2, sticky=(tk.N, tk.S))
-
-        confirm_button.pack()
-
+        back_button.place(rely=0.85, relx=0.75)
+        label_lamp.place(rely=0.80, relx=0.6)
+        go_monitor_button.place(rely=0.85, relx=0.1)
+        ready_button.place(rely=0.80, relx=0.37)
+        
+    def back_to_selection_frame(self):
+        if hasattr(self, 'check_frame') and self.check_frame:
+            self.check_frame.destroy()
+        self.create_selection_frame(self.data_list)
 
     def create_monitor_frame(self):
 
-        self.selection_frame.destroy()
+        if self.selection_frame:
+            self.selection_frame.destroy()
+        if self.check_frame:
+            self.check_frame.destroy()
 
         self.check_frame = tk.Frame(
             self.root, pady=5, padx=5, bd=2, bg="red")  # フレームを設定
