5ca8a63122dec466b14dd1925fd9500e7bf75631
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-19 16:24:08 +0900

    ロボット通信スレッドの統合
    スレッド自身は動かない

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from queue import Queue
 from GUIDesigner.GUIDesigner import GUIDesigner
+from RobotCommunicationHandler.RobotCommunicationHandler import RobotCommunicationHandler
 
 
 def test_gui():
@@ -9,5 +10,12 @@ def test_gui():
     gui.start_gui(send_queue, receive_queue)
 
 
+def test_communicaion():
+    send_queue = Queue()
+    receive_queue = Queue()
+    communication_handler = RobotCommunicationHandler()
+    communication_handler.communication_loop(send_queue, receive_queue)
+
+
 if __name__ == "__main__":
-    test_gui()
+    test_communicaion()
