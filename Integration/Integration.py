98722e0a21fb5f376ba5f5fcf56b1d28b26d922f
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-19 20:01:59 +0900

    GUIのテストと通信のテストを統合
    モニタ画面で操作ボタンを押すと、UR(のシミュレーション)に対してデータを送信

--- Integration/Integration.py
+++ Integration/Integration.py
@@ -7,7 +7,7 @@ from threading import Thread
 from RobotCommunicationHandler.test_ur import _test_ur
 
 
-TEST_FLAG = True
+TEST_UR_CONN = True
 
 
 class Integration:
@@ -17,7 +17,7 @@ class Integration:
         self.cfd_send_queue = Queue()
         self.send_queue = Queue()
         self.receiv_queue = Queue()
-        if (TEST_FLAG):
+        if (TEST_UR_CONN):
             self.test_ur = _test_ur(TEST_PORT1)
 
     def test_send(self):
@@ -25,6 +25,15 @@ class Integration:
             self.ur_send_queue.put(
                 {"target": TransmissionTarget.TEST_TARGET_1, "message": "test"})
             time.sleep(5)
+
+    def test_watching_send_queue(self):
+        while True:
+            if not self.send_queue.empty():
+                # send_queueから値を取り出す
+                send_data = self.send_queue.get()
+                self.ur_send_queue.put(
+                    {"target": TransmissionTarget.TEST_TARGET_1, "message": str(send_data)})
+            time.sleep(0.1)
 
     def main(self):
         communicationHandler = RobotCommunicationHandler()
@@ -34,10 +43,10 @@ class Integration:
             args=(self.ur_send_queue, self.cfd_send_queue))
         self.communication_thread.start()
 
-        if (TEST_FLAG):
+        if (TEST_UR_CONN):
             test_ur_thread = Thread(target=self.test_ur.start)
             test_ur_thread.start()
-            test_send_thread = Thread(target=self.test_send)
-            test_send_thread.start()
-            
+            test_send_thread = Thread(target=self.test_watching_send_queue)
+
+        test_send_thread.start()
         guiDesigner.start_gui(self.send_queue, self.receiv_queue)
