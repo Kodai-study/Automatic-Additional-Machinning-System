4b98bd0eb7a88f5680277ab67d79ad4397d0999a
Koda-study <anchor.kou@softbank.ne.jp>
2023-10-24 18:29:36 +0900

    ロボットの一連の動作を作成

--- RobotCommunicationHandler/ProcessingReportOperation.py
+++ RobotCommunicationHandler/ProcessingReportOperation.py
@@ -2,14 +2,22 @@
 import socket
 import time
 
-operation_start = "WRK 0,TAP_FIN"
-move_end_machining = "SIG ATT_DRL_READY"
-ejject_attatch = "EJCT 0,ATTACH"
-
+cmd_operation_start = "WRK 0,TAP_FIN\n"
+cmd_move_end_machining = "SIG ATT_DRL_READY"
+cmd_eject_attatch = "EJCT 0,ATTACH\n"
+cmd_eject_detach = "EJCT 0,DETACH\n"
+"""
+WRK 0,TAP_FIN
+EJCT 0,ATTACH
+EJCT 0,DETACH
+EJCT 0,ATTACH
+EJCT 0,DETACH
+WRK 0,TAP_FIN
+"""
 # ソケット通信で受け取った文字列を引数と比較する関数
 
 
-def compare_string(ur_socket: socket.socket, compare_string: str):
+def wait_until_match_command(ur_socket: socket.socket, compare_string: str):
     while True:
         data = ur_socket.recv(1024)
         if data:
@@ -26,10 +34,18 @@ def send_input_command(ur_socket: socket.socket):
         data = data + "\n"
         if data == "bye":
             return
+        elif data == "start\n":
+            operation_sequence_start(ur_socket)
         ur_socket.sendall(data.encode())
 
 
 def operation_sequence_start(ur_socket: socket.socket):
-    ur_socket.sendall(operation_start.encode())
-    compare_string(ur_socket, move_end_machining)
-    ur_socket.sendall(move_end_machining.encode())
+    ur_socket.sendall(cmd_operation_start.encode())
+    wait_until_match_command(ur_socket, cmd_move_end_machining)
+    ur_socket.sendall(cmd_eject_attatch.encode())
+    time.sleep(10)
+    ur_socket.sendall(cmd_eject_detach.encode())
+    wait_until_match_command(ur_socket, cmd_move_end_machining)
+    ur_socket.sendall(cmd_eject_attatch.encode())
+    time.sleep(10)
+    ur_socket.sendall(cmd_eject_detach.encode())
