4ef7a15bcf36c0a84cb4e04b9e991a9ac04c6e2c
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-25 14:46:26 +0900

    コマンドによる動作の修正
    時間経過による実行から、既定のコマンドをロボットから受け取ってから動作をするように変更

--- RobotCommunicationHandler/ProcessingReportOperation.py
+++ RobotCommunicationHandler/ProcessingReportOperation.py
@@ -21,17 +21,20 @@ EJCT 0,ATTACH
 Main_Received: SIG DET
 EJCT 0,DETACH
 """
-# ソケット通信で受け取った文字列を引数と比較する関数
 
 
 def wait_until_match_command(ur_socket: socket.socket, compare_string: str):
+    """ソケット通信で受け取った文字列を引数と比較する関数
+        比較する文字列と一致するまでループを回す"""
     while True:
         data = ur_socket.recv(1024)
-        if data:
-            print(data.decode())
-            if data.decode() == compare_string:
-                print("Main_Received: ", compare_string)
-                break
+        if not data:
+            continue
+        data = data.decode()
+        print(data)
+        if data == compare_string:
+            print("Main_Received: ", compare_string)
+            break
 
 
 def send_input_command(ur_socket: socket.socket):
@@ -49,16 +52,26 @@ def send_input_command(ur_socket: socket.socket):
 
 def operation_sequence_start(ur_socket: socket.socket):
     ur_socket.sendall(cmd_operation_start.encode())
-    print("command_send: ", cmd_operation_start)
-    wait_until_match_command(ur_socket, cmd_move_end_machining)
+    print("command_send: ", cmd_operation_start)  # 加工が終了したことを通知
+
+    wait_until_match_command(
+        ur_socket, cmd_move_end_machining)  # 加工部のワークがある部分に移動
+
     ur_socket.sendall(cmd_eject_attatch.encode())
-    print("command_send: ", cmd_eject_attatch)
-    time.sleep(15)
-    ur_socket.sendall(cmd_eject_detach.encode())
+    print("command_send: ", cmd_eject_attatch)  # 吸着が完了→加工部から検査部へ移動
+
+    wait_until_match_command(ur_socket, cmd_eject_detach_ready)
+
+    ur_socket.sendall(cmd_eject_detach.encode())  # 吸着を解除
     print("command_send: ", cmd_eject_detach)
-    wait_until_match_command(ur_socket, cmd_move_imp_machining)
-    ur_socket.sendall(cmd_eject_attatch.encode())
+
+    wait_until_match_command(
+        ur_socket, cmd_move_imp_machining)  # 搬入部へ移動したことを通知
+
+    ur_socket.sendall(cmd_eject_attatch.encode())  # 吸着が完了したことを通知
     print("command_send: ", cmd_eject_attatch)
-    time.sleep(15)
-    ur_socket.sendall(cmd_eject_detach.encode())
+
+    wait_until_match_command(ur_socket, cmd_eject_detach_ready)
+
+    ur_socket.sendall(cmd_eject_detach.encode())  # 吸着を解除
     print("command_send: ", cmd_eject_detach)
