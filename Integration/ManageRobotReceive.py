d7fecdb70ba5be87a217564113667932c3a09b53
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-27 19:30:43 +0900

    コメントの修正

--- Integration/ManageRobotReceive.py
+++ Integration/ManageRobotReceive.py
@@ -5,8 +5,19 @@ from test_flags import TEST_UR_CONN
 
 
 class ManageRobotReceive:
+    """
+    ロボットから受け取ったメッセージを判別して、対応する処理を行うクラス
+
+    通信ソフトから取得したデータを、そのままhandle_receiv_message 関数に渡して呼び出す。
+    """
 
     def __init__(self, integration_instance):
+        """
+
+        Args:
+            integration_instance (Integration): 統合スレッドのインスタンス。
+            このインスタンスから、キューなどにアクセスする。
+        """
         self._integration_instance = integration_instance
         self._ur_command_handlers = {
             "SIG 0,ATT_IMP_READY": self._send_message_to_cfd,
@@ -28,28 +39,61 @@ class ManageRobotReceive:
         }
 
     def _send_message_to_cfd(self, message: str):
+        """
+        メッセージのハンドラ。CFDにメッセージを送信する
+
+        Args:
+            message (str): 送信するコマンド文字列
+        """
         # self._integration_instance.send_request_queue.put(
         #     {"target": TransmissionTarget.CFD, "message": message})
         print("send to cfd: ", message)
 
     def _send_message_to_ur(self, message: str):
+        """
+        メッセージのハンドラ。URにメッセージを送信する
+
+        Args:
+            message (str): 送信するコマンド文字列
+        """
         self._integration_instance.send_request_queue.put(
             {"target": TransmissionTarget.TEST_TARGET_1 if TEST_UR_CONN else TransmissionTarget.UR,
              "message": message})
         # print("send to ur: ", message)
 
     def _undefine(self, message: str):
+        """
+        ハンドラ。適切な処理がまだ割り当てられていないメッセージを受け取ったときに呼び出される
+        Args:
+            message (str): _description_
+        """
         print("undefined message : ", message)
 
     def _handle_connection_success(self, target: TransmissionTarget):
+        """接続成功時に呼び出されるハンドラ
+
+        Args:
+            target (TransmissionTarget): 接続成功した相手
+        """
         self._integration_instance.gui_request_queue.put(
             (GUISignalCategory.ROBOT_CONNECTION_SUCCESS, target))
 
     def _send_to_gui(self, message: str):
+        """GUIに、ロボットから受け取ったメッセージをそのまま送信する
+
+        Args:
+            message (str): ロボットから受け取ったメッセージ
+        """
         self._integration_instance.gui_request_queue.put(
             (RobotInteractionType.MESSAGE_RECEIVED, message))
 
     def handle_receiv_message(self, receiv_data: dict):
+        """
+        通信ソフトから受け取ったメッセージを判別して、対応する処理を行う
+
+        Args:
+            receiv_data (dict): 受け取ったメッセージの情報を格納した辞書
+        """
 
         # もし"msg_type"か"target"がなかったら、エラーを出力して終了
         if not receiv_data.get("msg_type") or not receiv_data.get("target"):
