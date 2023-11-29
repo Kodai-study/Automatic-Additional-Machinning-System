1bdb207b0dd59a173d45f814edbaf84220851d77
Kodai-study <anchor.kou@softbank.ne.jp>
2023-11-29 14:39:32 +0900

    画面の種類を表す列挙型に項目を追加

--- GUIDesigner/Frames.py
+++ GUIDesigner/Frames.py
@@ -4,3 +4,6 @@ from enum import Enum, auto
 class Frames(Enum):
     WAIT_CONNECTION = auto()
     LOGIN = auto()
+    EMERGENCY_STOP = auto()
+    WORK_REQUEST_OVERVIEW = auto()
+    TOOL_REQUEST_OVERVIEW = auto()
