aefcd7279b67d11e687d2859051ffc0be53a77f1
Kodai-study <anchor.kou@softbank.ne.jp>
2023-11-29 14:20:53 +0900

    ログイン画面の見た目の変更

--- GUIDesigner/screens/Login.py
+++ GUIDesigner/screens/Login.py
@@ -1,40 +1,54 @@
 import tkinter as tk
 from GUIDesigner.screens.ScreenBase import ScreenBase
 
+FORM_FONT_SIZE = 50
 
 class Login(ScreenBase):
-    def __init__(self, parent):
+    def __init__(self, parent: tk.Tk):
         super().__init__(parent)
-        # self.pack(fill="both", expand=True)
 
-        self.username_label = tk.Label(
-            self, text="ID:", font=("AR丸ゴシック体M", 24))
-        self.username_label.place(relx=0.5, rely=0.5, anchor="center")
         # 位置を中央上部に割合で指定
+        self._create_widgets(parent)
+        self.grid(sticky="nsew")
 
+    def _create_widgets(self, parent: tk.Tk):
+        parent.grid_columnconfigure(0, weight=1)
+        parent.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
 
+        # Configure the row heights
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+
+        # ID label
+        self.username_label = tk.Label(
+            self, text="ID:", font=("Arial", FORM_FONT_SIZE))
+        self.username_label.grid(row=0, column=0, sticky="e")
+
+        # ID entry
         self.username_entry = tk.Entry(
-            self, font=("AR丸ゴシック体M", 24))
-        self.username_entry.pack()
-        self.username_entry.place(relx=0.5, rely=0.5, anchor="center")
+            self, font=("Arial", FORM_FONT_SIZE))
+        # Increased vertical padding
+        self.username_entry.grid(row=0, column=1, sticky="ew", ipady=10)
 
+        # Password label
         self.password_label = tk.Label(
-            self, text="pass:", font=("AR丸ゴシック体M", 24))
-        self.password_label.pack()
-        self.password_label.place(relx=0.5, rely=0.5, anchor="center")
+            self, text="Pass:", font=("Arial", FORM_FONT_SIZE))
+        self.password_label.grid(row=1, column=0, sticky="e")
 
+        # Password entry
         self.password_entry = tk.Entry(
-            self, show="*", font=("AR丸ゴシック体M", 24))
-        self.password_entry.pack()
-        self.password_entry.place(relx=0.5, rely=0.5, anchor="center")
+            self, show="*", font=("Arial", FORM_FONT_SIZE))
+        # Increased vertical padding
+        self.password_entry.grid(row=1, column=1, sticky="ew", ipady=10)
 
-        self.error_label = tk.Label(self, text="IDかパスワードが間違っています",
-                                    font=("AR丸ゴシック体M", 24), fg="red")
-
+        # Login button
         login_button = tk.Button(
-            self, text="Login", command=self._perform_login, font=("AR丸ゴシック体M", 21))
-        login_button.pack()
-        login_button.place(x=1270, y=550)
+            self, text="Login", font=("Arial", FORM_FONT_SIZE))
+        login_button.grid(row=2, column=0, columnspan=3, sticky="ew", padx=200)
 
     def create_frame(self):
         self.tkraise()
