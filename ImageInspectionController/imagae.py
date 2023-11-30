dc9e09d30606b7d7279c372b53a44b2004c4038e
Kazuaki-kuga <145450910+Kazuaki-kuga@users.noreply.github.com>
2023-11-30 16:12:56 +0900

    円と四角形の検出

--- ImageInspectionController/imagae.py
+++ ImageInspectionController/imagae.py
@@ -2,46 +2,42 @@ import cv2
 import numpy as np
 
 def detect_circles(image_path):
-    # 画像の読み込み
     image = cv2.imread(image_path)
 
-    # グレースケールに変換
+    # グレースケール変換
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # 画像のぼかしを適用（ノイズの軽減のため）
-    gray = cv2.medianBlur(gray, 5)
+    # ガウシアンブラーによるノイズ低減
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    # ハフ変換を使用して円を検出
+    # Cannyエッジ検出
+    edges = cv2.Canny(blurred, 50, 150)
+
     circles = cv2.HoughCircles(
-        gray,
+        edges,
         cv2.HOUGH_GRADIENT,
         dp=1,
-        minDist=30,
-        param1=100,
-        param2=45,
-        minRadius=1,
-        maxRadius=1000
+        minDist=50,
+        param1=50,
+        param2=30,
+        minRadius=20,
+        maxRadius=50
     )
 
     detected_circles = []
 
     if circles is not None:
-        # 検出された円を整数に変換
         detected_circles = np.uint16(np.around(circles))
 
     return detected_circles
 
+# 以下の部分は変更がありません
+
 def detect_squares(image_path):
-    # 画像の読み込み
     image = cv2.imread(image_path)
-
-    # グレースケールに変換
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # 画像のぼかしを適用（ノイズの軽減のため）
     gray = cv2.medianBlur(gray, 5)
 
-    # 四角形を検出
     gray_canny = cv2.Canny(gray, 50, 150)
     contours, _ = cv2.findContours(gray_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -51,44 +47,49 @@ def detect_squares(image_path):
         epsilon = 0.02 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
 
-        if len(approx) == 4:  # 近似された輪郭が四角形である場合
+        if len(approx) == 4:
             detected_squares.append(approx)
 
     return detected_squares
 
+def measure_dimensions(square):
+    side1 = np.linalg.norm(square[0] - square[1])
+    side2 = np.linalg.norm(square[1] - square[2])
+    
+    return side1, side2
+
 def main():
-    image_path = 'ImageInspectionController/img3.png'
+    image_path = 'ImageInspectionController/img21.png'
 
-    # 円の検出
     detected_circles = detect_circles(image_path)
-
-    # 四角形の検出
     detected_squares = detect_squares(image_path)
 
-    # 画像の読み込み
     image = cv2.imread(image_path)
 
-    # 検出された円を赤色で描画
     if len(detected_circles) > 0:
-        for circle in detected_circles[0, :]:
+        for i, circle in enumerate(detected_circles[0, :]):
             center = (circle[0], circle[1])
             radius = circle[2]
             cv2.circle(image, center, radius, (0, 0, 255), 2)
-            
-            # 円の中心を描画
             cv2.circle(image, center, 2, (0, 255, 0), 3)
+            print(f"円 {i + 1}: 中心 = {center}, 半径 = {radius}")
     else:
-        print('円が見つかりませんでした。')
+        print('円が見つかりませんでした.')
 
-    # 検出された四角形を緑色で描画
     if len(detected_squares) > 0:
-        for square in detected_squares:
-            cv2.polylines(image, [square], True, (0, 255, 0), 2)
+        for i, square in enumerate(detected_squares):
+            print(f"四角形 {i + 1}: ", end="")
+            colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
+            for j, point in enumerate(square):
+                color = colors[j]
+                cv2.circle(image, tuple(point[0]), 5, color, -1)
+            print()
+            side1, side2 = measure_dimensions(square)
+            print(f"  縦の寸法: {side1}, 横の寸法: {side2}")
     else:
-        print('四角形が見つかりませんでした。')
+        print('四角形が見つかりませんでした.')
 
-    # 結果の表示
-    cv2.imshow('Detected Shapes', image)
+    cv2.imshow('検出された形状', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
