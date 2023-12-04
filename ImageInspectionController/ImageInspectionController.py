72728a740c2b686d596377f0fc4cbcb06aae5574
Kazuaki-kuga <145450910+Kazuaki-kuga@users.noreply.github.com>
2023-12-04 10:46:55 +0900

    撮影(動かない)

--- ImageInspectionController/ImageInspectionController.py
+++ ImageInspectionController/ImageInspectionController.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
-from ImageInspectionController import OperationType
+from ImageInspectionController.OperationType import OperationType
 from ImageInspectionController.InspectDatas import PreProcessingInspectionData, ToolInspectionData
 from ImageInspectionController.InspectionResults import CameraControlResult, LightningControlResult, PreProcessingInspectionResult, ToolInspectionResult
-from ImageInspectionController.ProcessDatas import HoleCheckInfo
+from ImageInspectionController.pre_PROCESSING_INSPECTION import process_qr_code
+from ImageInspectionController.ProcessDatas import HoleCheckInfo, InspectionType
+from ImageInspectionController.Taking import Taking
 from common_data_type import CameraType, LightingType
 from typing import Tuple, Union, List
 
@@ -11,10 +13,11 @@ from typing import Tuple, Union, List
 class ImageInspectionController:
 
     def __init__(self):
-        pass
+        self.taking = Taking()
 
-    def perform_image_operation(self, operation_type: OperationType, inspection_data: Union[PreProcessingInspectionData, ToolInspectionData, List[HoleCheckInfo], Tuple(LightingType, bool), List(CameraType)]) \
-            -> Union[PreProcessingInspectionResult, ToolInspectionResult, List[HoleCheckInfo], LightningControlResult, CameraControlResult]:
+    def perform_image_operation(self, operation_type: OperationType, inspection_data: Union[PreProcessingInspectionData, ToolInspectionData, List[HoleCheckInfo], Tuple[LightingType, bool], List[CameraType]]) \
+           -> Union[PreProcessingInspectionResult, ToolInspectionResult, List[HoleCheckInfo], LightningControlResult, CameraControlResult]:
+ 
         """
         画像検査を行って、結果を返す関数。
         Args:
@@ -24,4 +27,21 @@ class ImageInspectionController:
         Returns:
             Union[PreProcessingInspectionResult, ToolInspectionResult, List[HoleCheckInfo]]: 検査の結果。合否と、検査で出された様々な値。検査の種類によって型が異なる
         """
-        pass
+        if operation_type==OperationType.PRE_PROCESSING_INSPECTION:
+            img_pass = self.taking.take_picuture(InspectionType.PRE_PROCESSING_INSPECTION)
+            kekka = process_qr_code(img_pass)
+        if operation_type==OperationType.ACCURACY_INSPECTION:
+            img_pass = self.taking.take_picuture(InspectionType.ACCURACY_INSPECTION)
+            kekka = (img_pass)
+        if operation_type==OperationType.TOOL_INSPECTION:
+            img_pass = self.taking.take_picuture(InspectionType.TOOL_INSPECTION)
+            kekka = (img_pass)
+        
+        return kekka 
+    
+    def test(kensamei:InspectionType):
+        img_pass=Taking.take_picuture(kensamei)
+        
+        return img_pass       
+    
+     
---
+++ ImageInspectionController/Taking.py
@@ -0,0 +1,94 @@
+#
+# grab_software_trigger_ndarray.py (for Python 3)
+#
+# Copyright (c) 2020 Toshiba-Teli Corporation
+#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+#
+
+import sys
+import numpy as np
+import pytelicam
+import yaml
+import cv2
+from ImageInspectionController.ProcessDatas import InspectionType
+from ImageInspectionController.light import Light
+
+
+class Taking:
+    def take_picuture(self,kensamei:InspectionType)->str:
+        file_path = 'ImageInspectionController/kensa_config.yaml'
+        with open(file_path, 'r') as yaml_file:
+            data = yaml.safe_load(yaml_file)
+        if InspectionType == InspectionType.TOOL_INSPECTION:
+            kougucamera_info = data['Camera_information']['TOOL_INSPECTION']
+            serial_number = kougucamera_info['serial_number']
+            model_number = kougucamera_info['model_number']
+        # アクセス例
+        # kougucamera_info = data['Camera_information']['TOOL_INSPECTION']
+        # serial_number = kougucamera_info['serial_number']
+        # kataban = kougucamera_info['kataban']
+        # print("Serial Number:", serial_number)
+        # print("Kataban:", kataban)
+        cam_system=self.initial_cam_setting(1)
+        cam_device,receive_signal=self.setting_cam(serial_number,model_number,cam_system)
+        np_arr = self.take_picture(cam_device,cam_system,receive_signal)
+        # print('shape      : {0}'.format(np_arr.shape))
+        # print('image data :\n {0}'.format(np_arr[0,]))
+        # print('average    : {0}\n'.format(np.average(np_arr)))
+        # グレースケール画像の保存
+        cv2.imwrite('grayscale_image.png', np_arr)
+        return 'grayscale_image.png'
+    # It is recommended that the settings of unused interfaces be removed.
+    #  (U3v / Gev / GenTL)
+    def initial_cam_setting(cam_num=3)->pytelicam.pytelicam.CameraSystem:
+        cam_system = pytelicam.get_camera_system( \
+        int(pytelicam.CameraType.U3v) | \
+        int(pytelicam.CameraType.Gev))
+        find_cam_num = cam_system.get_num_of_cameras()
+        if find_cam_num != cam_num:
+            print('Camera number is No')
+            return None
+        return cam_system
+
+    def setting_cam(serial_num:str,model:str,cam_system:pytelicam.pytelicam.CameraSystem)\
+        ->(pytelicam.pytelicam.CameraDevice,pytelicam.pytelicam.SignalHandle):
+        cam_device = cam_system.create_device_object_from_info(serial_num,model,"")
+        cam_device.open()
+
+        res = cam_device.genapi.set_enum_str_value('TriggerMode', 'On')
+        if res != pytelicam.CamApiStatus.Success:
+            raise Exception("Can't set TriggerMode.")
+
+        res = cam_device.genapi.set_enum_str_value('TriggerSource', 'Software')
+        if res != pytelicam.CamApiStatus.Success:
+            raise Exception("Can't set TriggerSource.")
+
+        res = cam_device.genapi.set_enum_str_value('TriggerSequence', 'TriggerSequence0')
+        receive_signal = cam_system.create_signal()
+
+        cam_device.cam_stream.open(receive_signal)
+        return cam_device,receive_signal
+
+    def take_picture(cam_device,cam_system,receive_signal) -> np.ndarray:
+
+        cam_device.cam_stream.start()
+
+
+        res = cam_device.genapi.execute_command('TriggerSoftware')
+        if res != pytelicam.CamApiStatus.Success:
+            raise Exception("Can't execute TriggerSoftware.")
+
+        res = cam_system.wait_for_signal(receive_signal)
+        if res != pytelicam.CamApiStatus.Success:
+            print('Grab error! status = {0}'.format(res))
+            return 
+        else:
+            # If you don't use 'with' statement, image_data.release() must be called after using image_data.
+            with cam_device.cam_stream.get_current_buffered_image() as image_data:
+                if image_data.status != pytelicam.CamApiStatus.Success:
+                    print('Grab error! status = {0}'.format(image_data.status))
+                    return
+                else:
+                    return image_data.get_ndarray(pytelicam.OutputImageType.Bgr24)  
