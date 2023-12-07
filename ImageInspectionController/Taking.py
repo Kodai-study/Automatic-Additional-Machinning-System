9ca87fc9148a4fbb97a54cabae16ea924545c9d5
Koda-study <anchor.kou@softbank.ne.jp>
2023-12-07 19:28:18 +0900

    bugfix: カメラ撮影の時の照明の操作を直した

--- ImageInspectionController/Taking.py
+++ ImageInspectionController/Taking.py
@@ -3,27 +3,30 @@ from ImageInspectionController.light import Light
 
 
 class Taking:
+    def __init__(self):
+        self.light = Light()
+
     def take_picuture(self, kensamei: InspectionType) -> str:
         self._test_takepicture_ok(kensamei)
 
     def _test_takepicture_ok(self, kensamei: InspectionType) -> str:
         if (kensamei == InspectionType.PRE_PROCESSING_INSPECTION):
             ONorOFF = "ON"
-            houkoku = Light.light_on(kensamei, ONorOFF)
+            houkoku = self.light.light_on(kensamei, ONorOFF)
             if (houkoku == "OK"):
                 print("satuei")
                 return "ImageInspectionController/QR.png"
 
         elif kensamei == InspectionType.ACCURACY_INSPECTION:
             ONorOFF = "ON"
-            houkoku = Light.light_on(kensamei, ONorOFF)
+            houkoku = self.light.light_on(kensamei, ONorOFF)
             if (houkoku == "OK"):
                 print("satuei")
                 return "ImageInspectionController/test/ana.png"
 
         elif kensamei == InspectionType.TOOL_INSPECTION:
             ONorOFF = "ON"
-            houkoku = Light.light_on(kensamei, ONorOFF)
+            houkoku = self.light.light_on(kensamei, ONorOFF)
             if (houkoku == "OK"):
                 print("satuei")
                 return "ImageInspectionController/test/drill.png"
