a78b244197f0d8767907f0b74649851413b3d66c
Kazuaki-kuga <ktoripuru460@gmail.com>
2023-12-12 09:28:20 +0900

    精度検査テスト(途中)

--- ImageInspectionController/test/imagae.py
+++ ImageInspectionController/test/imagae.py
@@ -1,11 +1,7 @@
-# coding: utf-8
-#画像パス
-#返り値
-#画像処理を施した画像、良否判定
 import cv2
 import numpy as np
 
-def detect_circles(image_path):
+def detect_circles(image_path, max_circles=5):
     image = cv2.imread(image_path)
 
     # グレースケール変換
@@ -18,111 +14,68 @@ def detect_circles(image_path):
     edges = cv2.Canny(blurred, 20, 50)
     edges = cv2.bitwise_not(edges)
     cv2.imshow("Edge Detection", edges)
-    cv2.waitKey(0)
     circles = cv2.HoughCircles(
         edges,
         cv2.HOUGH_GRADIENT,
         dp=1,
         minDist=50,
-        param1=2000,
-        param2=150,
+        param1=50,
+        param2=30,
         minRadius=20,
         maxRadius=109
     )
 
-    for i, circle in enumerate(circles[0, :]):
-        center = (circle[0], circle[1])
-        radius = circle[2]
-        cv2.circle(image, center, radius, (0, 0, 255), 2)
-        cv2.circle(image, center, 2, (0, 255, 0), 3)
-        print(f"円 {i + 1}: 中心 = {center}, 半径 = {radius}")
-
-    cv2.imshow('検出された形状', image)
-    cv2.waitKey(0)
-    for max_radius in range(30, 201):
-        circles = cv2.HoughCircles(
-            edges,
-            cv2.HOUGH_GRADIENT,
-            dp=1,
-            minDist=50,
-            param1=50,
-            param2=30,
-            minRadius=20,
-            maxRadius=max_radius
-        )
-        if  not circles is None:
-            key=0
-        # if key != -1:
-        #     break
-
-
     detected_circles = []
 
     if circles is not None:
-        detected_circles = np.uint16(np.around(circles))
+        circles = np.uint16(np.around(circles))
+        for i, circle in enumerate(circles[0, :]):
+            if len(detected_circles) >= max_circles:
+                break  # 検出する円の最大数に達したら終了
 
-    return detected_circles
-
-# 以下の部分は変更がありません
-
-def detect_squares(image_path):
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(gray, 5)
-
-    gray_canny = cv2.Canny(gray, 50, 150)
-    contours, _ = cv2.findContours(gray_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    detected_squares = []
-
-    for contour in contours:
-        epsilon = 0.02 * cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, epsilon, True)
-
-        if len(approx) == 4:
-            detected_squares.append(approx)
-
-    return detected_squares
-
-def measure_dimensions(square):
-    side1 = np.linalg.norm(square[0] - square[1])
-    side2 = np.linalg.norm(square[1] - square[2])
-    
-    return side1, side2
-
-def main():
-    image_path = '/home/kuga/img/c.png'
-
-    detected_circles = detect_circles(image_path)
-    detected_squares = detect_squares(image_path)
-
-    image = cv2.imread(image_path)
-
-    if len(detected_circles) > 0:
-        for i, circle in enumerate(detected_circles[0, :]):
             center = (circle[0], circle[1])
-            radius = circle[2]
-            cv2.circle(image, center, radius, (0, 0, 255), 2)
-            cv2.circle(image, center, 2, (0, 255, 0), 3)
-            print(f"円 {i + 1}: 中心 = {center}, 半径 = {radius}")
-    else:
-        print('円が見つかりませんでした.')
+            radius = int(circle[2])  # 半径を整数に変換
 
-    if len(detected_squares) > 0:
-        for i, square in enumerate(detected_squares):
-            print(f"四角形 {i + 1}: ", end="")
-            colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
-            for j, point in enumerate(square):
-                color = colors[j]
-                cv2.circle(image, tuple(point[0]), 5, color, -1)
-            print()
-            side1, side2 = measure_dimensions(square)
-            print(f"  縦の寸法: {side1}, 横の寸法: {side2}")
-    else:
-        print('四角形が見つかりませんでした.')
+            # 既存の円との重なりを確認
+            overlap = False
+            for existing_circle in detected_circles:
+                existing_center = existing_circle["center"]
+                existing_radius = existing_circle["radius"]
+                distance = np.sqrt((center[0] - existing_center[0])**2 + (center[1] - existing_center[1])**2)
+                if distance < (radius + existing_radius):
+                    overlap = True
+                    break
+
+            if not overlap:
+                cv2.circle(image, center, radius, (0, 0, 255), 2)
+                cv2.circle(image, center, 2, (0, 255, 0), 3)
+                detected_circles.append({
+                    "center": center,
+                    "radius": radius,
+                })
+                print(f"円 {i + 1}: 中心 = {center}, 半径 = {radius}")
+
+    # 画像を保存
+    output_image_path = 'ImageInspectionController/test/output_circles.png'
+    cv2.imwrite(output_image_path, image)
 
     cv2.imshow('検出された形状', image)
     cv2.waitKey(0)
+
+    return detected_circles, output_image_path
+
+def main():
+    image_path = 'ImageInspectionController/test/c.png'
+    max_circles_to_detect = 5  # 検出する円の最大数
+
+    detected_circles, output_image_path = detect_circles(image_path, max_circles_to_detect)
+
+    for i, circle in enumerate(detected_circles):
+        center = circle["center"]
+        radius = circle["radius"]
+        print(f"円 {i + 1}: 中心座標 = {center}, 直径 = {radius * 2} px")
+
+    print(f"検出結果の画像を保存しました: {output_image_path}")
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
