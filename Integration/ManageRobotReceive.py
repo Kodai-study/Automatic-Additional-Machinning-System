72558a53b61627dc9223f47beb6ff64cc7beb829
Kodai-study <anchor.kou@softbank.ne.jp>
2023-12-20 10:31:33 +0900

    fix: エジェクタの吸着・脱着命令をGUIに反映

--- Integration/ManageRobotReceive.py
+++ Integration/ManageRobotReceive.py
@@ -186,13 +186,13 @@ class ManageRobotReceive:
             return self._undefine(command)
 
         if target == TransmissionTarget.CFD or target == TransmissionTarget.TEST_TARGET_2:
-            return lambda: _send_message_to_ur(command, self._integration_instance.send_request_queue)
-        elif target == TransmissionTarget.UR or target == TransmissionTarget.TEST_TARGET_1:
             def _handler():
-                _send_message_to_cfd(command, self._integration_instance.send_request_queue)
-                self._integration_instance.robot_status[""]
+                _send_message_to_ur(command, self._integration_instance.send_request_queue)
+                self._integration_instance.robot_status["ejector"] = (detail == "ATTACH")
                 notice_change_status(self._integration_instance.gui_request_queue)
             return _handler
+        elif target == TransmissionTarget.UR or target == TransmissionTarget.TEST_TARGET_1:
+            return lambda: _send_message_to_cfd(command, self._integration_instance.send_request_queue)
 
     def _select_handler_sensor(self, dev_num: int, detail: str, command: str, serial_number: int = None, target: TransmissionTarget = None):
         """
