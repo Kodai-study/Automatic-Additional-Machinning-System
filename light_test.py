2231fe610be0ad61ad01412f7638520cc38c0d7e
Kazuaki-kuga <ktoripuru460@gmail.com>
2023-12-26 10:43:51 +0900

    標準入力からlightをコントロールする

---
+++ light_test.py
@@ -0,0 +1,29 @@
+import serial.tools.list_ports
+
+IOBOARD_PID = 2050
+IOBOARD_VID = 10777
+
+# COMポートの一覧を取得
+ports = list(serial.tools.list_ports.comports())
+com_num = None
+
+for port in ports:
+    if port.pid == IOBOARD_PID and port.vid == IOBOARD_VID:
+        com_num = port.device
+
+if com_num is None:
+    print("COMポートが見つかりませんでした。")
+    exit()
+
+else:
+    serPort = serial.Serial(com_num, 19200, timeout=1)
+    print(f"COMポート {com_num} を使用します。")
+
+while True:
+    str = input()
+    if (str == "bye"):
+        break
+
+    serPort.write((str + "\r").encode('utf-8'))
+    print((str + "\r"))
+
