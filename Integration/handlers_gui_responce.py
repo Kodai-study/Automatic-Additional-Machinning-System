59fbf7ba47fceea55b0a032d1e28556259d7453a
Kodai-study <anchor.kou@softbank.ne.jp>
2024-01-28 18:56:16 +0900

    ✨ refact: 加工データ確定の受け取り側を作成
    加工データ確定を受け取ると、ロボットにコマンドを送信

--- Integration/handlers_gui_responce.py
+++ Integration/handlers_gui_responce.py
@@ -54,15 +54,19 @@ class GuiResponceHandler:
         elif send_data[0] == GUIRequestType.REQUEST_PROCESSING_DATA_LIST:
             self._get_process_data_list()
 
+        elif send_data[0] == GUIRequestType.UPLOAD_PROCESSING_DETAILS:
+            self.send_request_queue.put(
+                {"target": TransmissionTarget.CFD if TEST_CFD_CONNECTION_LOCAL else TransmissionTarget.TEST_TARGET_2, "message": "MODE 0,RESERVE_SET"})
+
     def _login(self, id: str, password: str, database_accesser: DBAccessHandler):
         sql = f"""
         SELECT t_login.employee_no, t_login.password, t_login.authority,
             m_employee.employee_name, m_employee.affiliation
         FROM t_login
         JOIN m_employee ON t_login.employee_no = m_employee.employee_no
-        WHERE t_login.employee_no = {id} AND t_login.password = {password}
+        WHERE t_login.employee_no = %s AND t_login.password = %s
         """
-        result = database_accesser.fetch_data_from_database(sql)
+        result = database_accesser.fetch_data_from_database(sql, id, password)
         if len(result) != 1:
             self.gui_request_queue.put(
                 GUIRequestType.LOGIN_REQUEST, {"is_success": False})
